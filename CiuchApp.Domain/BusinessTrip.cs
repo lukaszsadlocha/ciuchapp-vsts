b962384fe9fb0559fd79fdd57de4828520d3b689
Unknown <lukaszsadlocha@gmail.com>
2018-08-28 13:12:24 +0200

    Add Data annotation (and attributes) to models to improve views

--- CiuchApp.Domain/BusinessTrip.cs
+++ CiuchApp.Domain/BusinessTrip.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CiuchApp.Domain
 {
     public class BusinessTrip
     {
+        [DisplayName("Numer")]
+//        [CiuchAppTranslatable]
         public int Id { get; set; }
+
+        [DisplayName("Data od")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateFrom { get; set; }
+
+        [DisplayName("Data do")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateTo { get; set; }
-        public Country Country { get; set; }
-        public string CountryId { get; set; }
-        public City City { get; set; }
+        public virtual Country Country { get; set; }
+        public int CountryId { get; set; }
+        [DisplayName("Miasto")]
+        public virtual City City { get; set; }
         public int CityId { get; set; }
-        public Season Season { get; set; }
+        public virtual Season Season { get; set; }
         public int SeasonId { get; set; }
-        public Currency Currency { get; set; }
+        public virtual Currency Currency { get; set; }
         public int CurrencyId { get; set; }
 
         public static string JsonKey = nameof(BusinessTrip)+"Json";
